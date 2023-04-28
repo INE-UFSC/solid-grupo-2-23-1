03b93fd545bdec625ea14199bf92f70d4a3ff319
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2023-04-28 11:29:21 -0300

    Adicionar arquivo quatro

--- 4.isp.py
+++ 4.isp.py
@@ -4,43 +4,34 @@ Crie interfaces que são específicas. Clientes não devem depender de interface
 """
 from abc import ABC
 
+# Removi os métodos desnecessários em IJanela e deixei somente os necessários
+# para as duas classes.
+
 class IJanela(ABC):
-    def maximizar(self):
-        raise NotImplementedError
-    
     def minimizar(self):
         raise NotImplementedError
-    
-    def mostrar_menu(self):
-        raise NotImplementedError
-        
+
     def fechar(self):
         raise NotImplementedError
+
 
 class JanelaTamanhoFixo(IJanela):
-    def maximizar(self):
-        pass
-
     def minimizar(self):
-        pass
-    
-    def mostrar_menu(self):
         pass
     
     def fechar(self):
         pass
+
+    def mostrar_menu(self):
+        pass
+    
 
 class JanelaSemMenu(IJanela):
-    def maximizar(self):
-        pass
-
     def minimizar(self):
         pass
-    
-    def mostrar_menu(self):
-        pass
-    
+
     def fechar(self):
         pass
 
-
+    def maximizar(self):
+        pass
